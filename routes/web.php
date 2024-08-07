9636a5861565cd47f6b9d13648ff5132ba190d10
Moaziz667 <159254604+Moaziz667@users.noreply.github.com>
2024-08-07 20:49:53 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WeatherController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/weather', [WeatherController::class, 'index'])->name('weather.index');
+
+Route::get('/', [WeatherController::class, 'index'])->name('weather.index');
